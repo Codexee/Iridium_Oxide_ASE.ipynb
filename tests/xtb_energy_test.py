1b6c2ab3b2a1865e1f357964c3f21ad45cd2c89e
sapphire10-k <manisha@westward.academy>
2025-12-21 22:11:50 +0000

    Update xtb_energy_test.py
    Fixed missing closing )

--- tests/xtb_energy_test.py
+++ tests/xtb_energy_test.py
@@ -5,7 +5,7 @@ def test_xtb_energy_h2():
   from ase import Atoms
   from xtb.ase.calculator import XTB
 
-  atoms = atoms("H2", positions[[0,0,0],[0,0,0]]
+  atoms = atoms("H2", positions[[0,0,0],[0,0,0]])
   atoms.calc = XTB(method="GFN2-xTB")
   e=atoms.get_potential_energy()
   assert e == e
