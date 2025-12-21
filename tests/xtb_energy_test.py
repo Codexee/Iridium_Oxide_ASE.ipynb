a219d4256ec95ff1cb9e71802ab9983c99013972
sapphire10-k <manisha@westward.academy>
2025-12-21 22:18:07 +0000

    Update xtb_energy_test.py
    Fixed missing = sign

--- tests/xtb_energy_test.py
+++ tests/xtb_energy_test.py
@@ -5,7 +5,7 @@ def test_xtb_energy_h2():
   from ase import Atoms
   from xtb.ase.calculator import XTB
 
-  atoms = Atoms("H2", positions[[0,0,0],[0,0,0]])
+  atoms = Atoms("H2", positions=[[0,0,0],[0,0,0]])
   atoms.calc = XTB(method="GFN2-xTB")
   e=atoms.get_potential_energy()
   assert e == e
